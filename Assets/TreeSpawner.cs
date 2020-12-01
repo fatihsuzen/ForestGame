73465af6b6c87d48920443dae648203a8e2c50ca
fatihsuzen <fatihsuzenbjk@gmail.com>
2020-12-01 23:47:16 +0300

    added forest

---
+++ Assets/TreeSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSpawner : MonoBehaviour
+{
+    public List<GameObject> Trees = new List<GameObject>();
+
+    private int GroundLenght=250;
+    void Start()
+    {
+        Invoke("TreeSpawn", 0);
+    }
+    public void TreeSpawn()
+    {
+        for(int i = 0;i<=2000;i++)
+        {
+            int PosX = Random.Range(-GroundLenght, GroundLenght);
+            int PosZ = Random.Range(-GroundLenght, GroundLenght);
+            int RandomTree = Random.Range(0, Trees.Count);
+            Instantiate<GameObject>(Trees[RandomTree], new Vector3(PosX,0,PosZ), Quaternion.identity);
+        }       
+    }
+}
